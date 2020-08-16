4aa002a10265af4a7e2206e3752f12fefa140a71
robertobirijnr <robertobirijnr@hotmail.com>
2020-08-16 04:22:31 +0000

    get single bootcamp

--- Controllers/bootCamp.js
+++ Controllers/bootCamp.js
@@ -16,11 +16,23 @@ exports.getBootcamps = async (req, res, next) => {
 
 
 
-exports.getBootcamp = (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        msg: "Show single bootcamp",
-    });
+exports.getBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findById(req.params.id);
+        if (!bootcamp) {
+            return res.status(400).json({
+                success: false
+            })
+        }
+        res.status(200).json({
+            success: true,
+            data: bootcamp
+        });
+    } catch (err) {
+        res.status(400).json({
+            success: false
+        })
+    }
 };
 
 exports.createBootcamp = async (req, res, next) => {
