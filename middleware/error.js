3a9c0e9971751c4b2f1ed258c4d59a5ebdae0823
robertobirijnr <robertobirijnr@hotmail.com>
2020-08-16 13:11:06 +0000

    duplicate error check

--- middleware/error.js
+++ middleware/error.js
@@ -13,6 +13,11 @@ const errorHandler = (err, req, res, next) => {
         error = new ErrorResponse(message, 404)
     }
 
+    if (err.code === 11000) {
+        const message = `Duplicate field value entered`;
+        error = new ErrorResponse(message, 400)
+    }
+
     res.status(error.statusCode || 500).json({
         success: false,
         error: error.message || 'Server Error'
