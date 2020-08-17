70f8adf225d5247a7f29df7aea2266c105ee5501
robertobirijnr <robertobirijnr@hotmail.com>
2020-08-17 20:54:18 +0000

    create course

--- Controllers/course.js
+++ Controllers/course.js
@@ -1,6 +1,7 @@
 const Course = require("../models/course");
 const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/asyncAwait");
+const Bootcamp = require("../models/bootCamp")
 
 
 exports.getCourses = asyncHandler(async (req, res, next) => {
@@ -24,5 +25,40 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
         success: true,
         count: courses.length,
         data: courses
+    })
+})
+
+exports.getSingleCourse = asyncHandler(async (req, res, next) => {
+    const course = await Course.findById(req.params.id).populate({
+        path: 'bootcamp',
+        select: 'name description'
+    })
+
+    if (!course) {
+        return next(new ErrorResponse(`No course found with the if of ${req.params.id}`), 404);
+    }
+
+    res.status(200).json({
+        success: true,
+        data: course
+    })
+})
+
+exports.createCourse = asyncHandler(async (req, res, next) => {
+    req.body.bootcamp = req.params.bootcampId;
+
+    const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+
+    if (!bootcamp) {
+        return next(
+            new ErrorResponse(`No bootcamp with the id of ${req.params.bootcampId}`, 404)
+        )
+    }
+
+    const course = await Course.create(req.body);
+
+    res.status(200).json({
+        success: true,
+        data: course
     })
 })
--- Routes/course.js
+++ Routes/course.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const {
-    getCourses
+    getCourses,
+    getSingleCourse,
+    createCourse
 } = require("../Controllers/course");
 
 const router = express.Router({
     mergeParams: true
 });
 
-router.route('/').get(getCourses)
+router.route('/').get(getCourses).post(createCourse)
+router.route('/:id')
+    .get(getSingleCourse)
 
 
 module.exports = router;
